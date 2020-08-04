9e942bf6e9de7ab8a90f710ebc6cc5770618ffda
Leonardo Telles <leo.c.telles@gmail.com>
2020-08-04 18:41:28 -0300

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import "./assets/styles/global.css";
 
-import Landing from "./pages/Landing";
+import Routes from "./pages/routes";
 
 function App() {
-  return <Landing />;
+  return <Routes />;
 }
 
 export default App;
