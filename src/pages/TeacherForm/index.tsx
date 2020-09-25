088445bb4a4d882ee5ae5be1c33d6db935ba5df7
Leonardo Telles <leo.c.telles@gmail.com>
2020-09-25 19:01:05 -0300

    Update TeacherForm

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Input from "../../components/Input";
 import PageHeader from "../../components/PageHeader";
+import Textarea from "../../components/Textarea";
+import Select from "../../components/Select";
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
 
@@ -17,8 +21,24 @@ function TeacherForm() {
           <legend>Seus dados</legend>
           <Input name="name" label="Nome completo"/>
           <Input name="avatar" label="Avatar"/>
-          <Input name="whatsapp" label="Whatsapp"/>        
+          <Input name="whatsapp" label="Whatsapp"/> 
+          <Textarea name="bio" label="Biografia" />       
         </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Select name="subject" label="Matéria"/>
+          <Input name="cost" label="Custo da sua hora por aula"/>               
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante"/>
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">Salvar cadastro</button>
+        </footer>
       </main>
     </div>
   );
